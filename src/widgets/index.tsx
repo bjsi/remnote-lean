afd209030b7dcf21af6751b465461f238096dd7e
jamesb <experimentallearning0@gmail.com>
2023-03-06 07:58:01 +0000

    refactor

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,6 +4,15 @@ import '../App.css';
 import { defaultProofText, leanProofPowerupCode } from '../lib/const';
 
 async function onActivate(plugin: ReactRNPlugin) {
+  await registerLeanProofPowerup(plugin);
+  await registerApplyLeanProofCommand(plugin);
+  await registerProofEditorWidget(plugin);
+  await registerOpenProofEditorWidget(plugin);
+}
+
+async function onDeactivate(_: ReactRNPlugin) {}
+
+async function registerLeanProofPowerup(plugin: ReactRNPlugin) {
   await plugin.app.registerPowerup(
     'Lean Proof',
     leanProofPowerupCode,
@@ -12,7 +21,9 @@ async function onActivate(plugin: ReactRNPlugin) {
       slots: [],
     }
   );
+}
 
+async function registerApplyLeanProofCommand(plugin: ReactRNPlugin) {
   await plugin.app.registerCommand({
     id: 'apply_lean_proof_powerup',
     name: 'Lean Proof',
@@ -21,23 +32,29 @@ async function onActivate(plugin: ReactRNPlugin) {
       if (focused) {
         await focused.addPowerup(leanProofPowerupCode);
         await focused.setBackText([defaultProofText]);
-        await plugin.window.openWidgetInPane('proof_editor', {
-          remId: focused._id,
-        });
+        await openProofEditorWidget(plugin, focused._id);
       }
     },
   });
+}
 
+async function registerProofEditorWidget(plugin: ReactRNPlugin) {
   await plugin.app.registerWidget('proof_editor', WidgetLocation.Pane, {
     dimensions: { height: 'auto', width: '100%' },
   });
+}
 
+async function registerOpenProofEditorWidget(plugin: ReactRNPlugin) {
   await plugin.app.registerWidget('open_proof_editor', WidgetLocation.RightSideOfEditor, {
     dimensions: { height: 'auto', width: 'auto' },
     powerupFilter: leanProofPowerupCode,
   });
 }
 
-async function onDeactivate(_: ReactRNPlugin) {}
+async function openProofEditorWidget(plugin: ReactRNPlugin, remId: string) {
+  await plugin.window.openWidgetInPane('proof_editor', {
+    remId: remId,
+  });
+}
 
 declareIndexPlugin(onActivate, onDeactivate);
